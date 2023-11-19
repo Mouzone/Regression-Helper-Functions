b8f4ca87920ba2e85bf1c46d74bd3ca0cd6f73f9
Mouzone <sunnyliu010@gmail.com>
2023-11-18 16:15:48 -0800

    unit tests for accuracy score

--- tests.py
+++ tests.py
@@ -1,20 +1,38 @@
-# test_my_module.py
+# test_main.py
 
 import unittest
-from my_module import add_numbers
+from main import accuracy_score
 
-class TestAddNumbersFunction(unittest.TestCase):
-    def test_add_numbers_positive_numbers(self):
-        result = add_numbers(3, 5)
-        self.assertEqual(result, 8)
+class TestAccuracyScoreFunction(unittest.TestCase):
+    def test_accuracy_score_all_correct(self):
+        y_true = [1, 1, 0, 0, 1]
+        y_predict = [1, 1, 0, 0, 1]
+        result = accuracy_score(y_true, y_predict)
+        self.assertEqual(result, 1.0)
 
-    def test_add_numbers_negative_numbers(self):
-        result = add_numbers(-2, -4)
-        self.assertEqual(result, -6)
+    def test_accuracy_score_all_incorrect(self):
+        y_true = [1, 1, 0, 0, 1]
+        y_predict = [0, 0, 1, 1, 0]
+        result = accuracy_score(y_true, y_predict)
+        self.assertEqual(result, 0.0)
 
-    def test_add_numbers_mixed_numbers(self):
-        result = add_numbers(10, -7)
-        self.assertEqual(result, 3)
+    def test_accuracy_score_mixed_predictions(self):
+        y_true = [1, 1, 0, 0, 1]
+        y_predict = [1, 0, 1, 0, 1]
+        result = accuracy_score(y_true, y_predict)
+        self.assertEqual(result, 0.6)
+
+    def test_accuracy_score_empty_input(self):
+        y_true = []
+        y_predict = []
+        result = accuracy_score(y_true, y_predict)
+        self.assertEqual(result, 0.0)
+
+    def test_accuracy_score_unequal_lengths(self):
+        y_true = [1, 0, 1]
+        y_predict = [0, 1]
+        with self.assertRaises(ValueError):
+            accuracy_score(y_true, y_predict)
 
 if __name__ == '__main__':
     unittest.main()
