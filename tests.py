b705c2aaee6d9adc107142fa4cc044a1ed94cc6a
Mouzone <sunnyliu010@gmail.com>
2023-11-18 16:30:02 -0800

    new tests for accuracy_score that also test error statements

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
-# test_main.py
+# test_accuracy_score.py
 
 import unittest
-from main import accuracy_score
+from main import accuracy_score  # Replace 'your_module' with the actual module name
 
 class TestAccuracyScoreFunction(unittest.TestCase):
     def test_accuracy_score_all_correct(self):
@@ -25,14 +25,16 @@ class TestAccuracyScoreFunction(unittest.TestCase):
     def test_accuracy_score_empty_input(self):
         y_true = []
         y_predict = []
-        result = accuracy_score(y_true, y_predict)
-        self.assertEqual(result, 0.0)
+        with self.assertRaises(ValueError) as context:
+            accuracy_score(y_true, y_predict)
+        self.assertEqual(str(context.exception), "No elements")
 
     def test_accuracy_score_unequal_lengths(self):
         y_true = [1, 0, 1]
         y_predict = [0, 1]
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ValueError) as context:
             accuracy_score(y_true, y_predict)
+        self.assertEqual(str(context.exception), "Mismatch in lengths 3 and 2")
 
 if __name__ == '__main__':
     unittest.main()
